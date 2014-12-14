aea330fff47e508062282cc621229912a6b8b6e1
Thomas Steur <thomas.steur@googlemail.com>
2014-12-14 23:47:57 +0100

    added an example test for NullCache

---
+++ tests/Backend/NullCacheTest.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL v3 or later
+ */
+
+namespace Tests\Piwik\Cache\Backend;
+
+use Piwik\Cache\Backend\NullCache;
+
+/**
+ * @covers \Piwik\Network\IPv4
+ */
+class NullCacheTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var NullCache
+     */
+    private $cache;
+
+    private $cacheId = 'testid';
+
+    public function setUp()
+    {
+        $this->cache = new NullCache();
+        $this->cache->doSave($this->cacheId, 'anyvalue', 100);
+    }
+
+    public function test_doSave_shouldAlwaysReturnTrue()
+    {
+        $this->assertTrue($this->cache->doSave('randomid', 'anyvalue', 100));
+    }
+
+    public function test_doFetch_shouldAlwaysReturnFalse_EvenIfSomethingWasSet()
+    {
+        $this->assertFalse($this->cache->doFetch($this->cacheId));
+    }
+
+    public function test_doContains_shouldAlwaysReturnFalse_EvenIfSomethingWasSet()
+    {
+        $this->assertFalse($this->cache->doContains($this->cacheId));
+    }
+
+    public function test_doDelete_shouldAlwaysPretendItWorked_EvenIfNoSuchKeyExists()
+    {
+        $this->assertTrue($this->cache->doDelete('loremipsum'));
+    }
+
+    public function test_doFlush_shouldAlwaysPretendItWorked()
+    {
+        $this->assertTrue($this->cache->doFlush());
+    }
+
+}
