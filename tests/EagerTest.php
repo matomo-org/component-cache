22140b9b986d960f1eb98342bc949ac742981beb
Stefan Giehl <stefan@matomo.org>
2025-01-08 16:08:11 +0100

    Migrate tests from travis to GitHub action (#37)
    * Create GitHub test action
    
    * Remove travis tests
    
    * start redis for tests
    
    * remove invalid attribute from phpunit.xml
    
    * use newer phpunit if needed
    
    * fix method declarations
    
    * convert expected exceptions

--- tests/EagerTest.php
+++ tests/EagerTest.php
@@ -32,7 +32,7 @@ class EagerTest extends TestCase
     private $cacheId    = 'testid';
     private $cacheValue = 'exampleValue';
 
-    protected function setUp()
+    protected function setUp(): void
     {
         $this->backend = new ArrayCache();
         $this->backend->doSave($this->storageId, array($this->cacheId => $this->cacheValue));
@@ -73,12 +73,10 @@ class EagerTest extends TestCase
         $this->assertSame($value, $this->cache->fetch($this->cacheId));
     }
 
-    /**
-     * @expectedException \InvalidArgumentException
-     * @expectedExceptionMessage cannot use this cache to cache an object
-     */
     public function test_save_shouldFail_IfTryingToSetAnObject()
     {
+        self::expectException(\InvalidArgumentException::class);
+        self::expectExceptionMessage('cannot use this cache to cache an object');
         $value = (object) array('anyotherE' => 'anyOtherValUE', 1 => array(2));
         $this->cache->save($this->cacheId, $value);
 
